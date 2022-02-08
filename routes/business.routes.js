592b915e8852419a18427c35530008835f52f4d1
Arthur Chastenet <arthur.chastenet@gmail.com>
2022-02-08 13:34:51 +0100

    Finished business and listing routes

--- routes/business.routes.js
+++ routes/business.routes.js
@@ -4,10 +4,13 @@ const UserModel = require("./../models/User.model");
 const uploader = require("./../config/cloudinary");
 const isAuthenticated = require("./../middlewares/jwt.middleware");
 
+// Set is Authenticated on every route so we can retrieve the user object in th req (Flo's method)
 router.use(isAuthenticated);
 
 router.get("/", (req, res, next) => {
   BusinessModel.find()
+    // Populate the listings so the we can use it in our listingsList component in the front
+    .populate("listings")
     .then((allBusinesses) => res.status(200).json(allBusinesses))
     .catch((err) => console.error(err));
 });
--- routes/listing.routes.js
+++ routes/listing.routes.js
@@ -2,12 +2,23 @@ const router = require("express").Router();
 const ListingModel = require("./../models/Listing.model");
 const BusinessModel = require("./../models/Business.model");
 
+// Get all listings
 router.get("/", (req, res, next) => {
   ListingModel.find()
     .then((allListings) => res.status(200).json(allListings))
     .catch((err) => next(err));
 });
 
+// Get one listing
+router.get("/:listingId", (req, res, next) => {
+  const { listingId } = req.params;
+
+  ListingModel.findById(listingId)
+    .then((listing) => res.status(200).json(listing))
+    .catch((err) => next(err));
+});
+
+// Create one listing
 router.post("/:businessId/create", async (req, res, next) => {
   try {
     // Retrieve the business Id from params
@@ -22,6 +33,53 @@ router.post("/:businessId/create", async (req, res, next) => {
     );
     // Send back the listing to the front
     res.status(200).json(createdListing);
+    // Else catch the error
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Delete one listing
+router.delete("/:businessId/delete/:listingId", async (req, res, next) => {
+  try {
+    const { listingId, businessId } = req.params;
+
+    const updatedBusinessObject = await BusinessModel.findByIdAndUpdate(
+      businessId,
+      {
+        $pull: { listings: listingId },
+      },
+      { new: true }
+    ).populate("listings");
+
+    await ListingModel.findByIdAndDelete(listingId);
+
+    res.status(202).json(updatedBusinessObject);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Upate one listing
+router.patch("/modify/:listingId", async (req, res, next) => {
+  try {
+    const { listingId } = req.params;
+    const { name, price, availableQuantity, description, recurring } = req.body;
+
+    console.log("This is req body / line 69 / listing Routes", req.body);
+
+    const updatedListing = await ListingModel.findByIdAndUpdate(
+      listingId,
+      { name, price, availableQuantity, description, recurring },
+      { new: true }
+    );
+
+    console.log(
+      "This is updatedListing / line 78 / listing routes",
+      updatedListing
+    );
+
+    res.status(204).json(updatedListing);
   } catch (error) {
     next(error);
   }
